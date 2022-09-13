f3baa19659282b0898828a185a9b77491037bc9e
Aakash Pandya <apandya@mobiquityinc.com>
2022-09-13 10:52:28 +0530

    added serve package

--- apps/pwa/src/registerServiceWorker.js
+++ apps/pwa/src/registerServiceWorker.js
@@ -1,13 +1,5 @@
 const registerServiceWorker = () => {
-  const isLocalhost = Boolean(
-    window.location.hostname === 'localhost' ||
-      window.location.hostname === '[::1]' ||
-      window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-      )
-  );
-
-  const isServiceWorkerSupported = 'serviceWorker' in navigator && !isLocalhost;
+  const isServiceWorkerSupported = 'serviceWorker' in navigator;
 
   if (!isServiceWorkerSupported) {
     console.log('Service workers are not supported.');
